35fcb100a42bcb4e6a15fa30be914a372483544f
Alexey Andreev <alexey.andreev@lykke.com>
2018-08-08 12:32:57 +0300

    MTC-272: changed OnBehalf flag checking to AdditionalInfo

--- src/MarginTrading.CommissionService.Services/CommissionCalcService.cs
+++ src/MarginTrading.CommissionService.Services/CommissionCalcService.cs
@@ -89,7 +89,7 @@ namespace MarginTrading.CommissionService.Services
             string accountAssetId)
         {
             var onBehalfEvents = (await _orderEventsApi.OrderById(orderId, null, false))
-                .Where(o => o.Originator == OriginatorTypeContract.OnBehalf).ToList();
+                .Where(CheckOnBehalfFlag).ToList();
 
             var changeEventsCount = onBehalfEvents.Count(o => o.UpdateType == OrderUpdateTypeContract.Change);
 
@@ -112,6 +112,20 @@ namespace MarginTrading.CommissionService.Services
             async Task<bool> CorrelatesWithParent(OrderEventContract order) =>
                 (await _orderEventsApi.OrderById(order.ParentOrderId, OrderStatusContract.Placed, false))
                 .Any(p => p.CorrelationId == order.CorrelationId);
+        }
+
+        private bool CheckOnBehalfFlag(OrderEventContract orderEvent)
+        {
+            //used to be o => o.Originator == OriginatorTypeContract.OnBehalf
+            try
+            {
+                return JsonConvert.DeserializeAnonymousType(orderEvent.AdditionalInfo, new {IsOnBehalf = false})
+                    .IsOnBehalf;
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
