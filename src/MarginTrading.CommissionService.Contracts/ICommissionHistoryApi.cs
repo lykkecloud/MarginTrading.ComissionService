b355841e1f5af705b4ab4f1edd67588131d6e5b7
Alexey Andreev <alexey.andreev@lykke.com>
2018-11-16 22:19:33 +0300

    MTC-444: history endpoint fixed

--- src/MarginTrading.CommissionService.Contracts/ICommissionHistoryApi.cs
+++ src/MarginTrading.CommissionService.Contracts/ICommissionHistoryApi.cs
@@ -16,17 +16,8 @@ namespace Lykke.MarginTrading.CommissionService.Contracts
         /// <summary>
         /// Retrieve overnight swap calculation history from storage between selected dates.
         /// </summary>
-        /// <param name="from"></param>
-        /// <param name="to"></param>
-        /// <returns></returns>
-        [Post("/api/overnightswap/history")]
-        Task<IEnumerable<OvernightSwapHistoryContract>> GetOvernightSwapHistory(
+        [Get("/api/overnightswap/history")]
+        Task<List<OvernightSwapHistoryContract>> GetOvernightSwapHistory(
             [Query] DateTime from, [Query] DateTime to);
-
-//        /// <summary>
-//        /// Invoke recalculation of account/instrument/direction order packages that were not calculated successfully last time.
-//        /// </summary>
-//        [Post("api/overnightswap/recalc.failed.orders")]
-//        Task RecalculateFailedOrders();
     }
 }
