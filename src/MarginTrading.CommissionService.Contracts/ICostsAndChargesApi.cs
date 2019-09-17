e117a43c099f824fc55ff1078093a97488175e70
Alexey Andreev <alexey.andreev@lykke.com>
2019-09-17 22:14:20 +0300

    LT-1637: make interface use REFIT attributes

--- src/MarginTrading.CommissionService.Contracts/ICostsAndChargesApi.cs
+++ src/MarginTrading.CommissionService.Contracts/ICostsAndChargesApi.cs
@@ -3,23 +3,31 @@
 
 using System;
 using System.Threading.Tasks;
+using JetBrains.Annotations;
 using Lykke.MarginTrading.CommissionService.Contracts.Models;
+using Refit;
 
 namespace Lykke.MarginTrading.CommissionService.Contracts
 {
+    [PublicAPI]
     public interface ICostsAndChargesApi
     {
+        [Post("/api/costsAndCharges")]
         Task<CostsAndChargesCalculationContract> GenerateSingle(string accountId, string instrument, decimal quantity,
             OrderDirectionContract direction, bool withOnBehalf);
         
+        [Post("/api/costsAndCharges/for-account")]
         Task<CostsAndChargesCalculationContract[]> GenerateForAccount(string accountId, bool withOnBehalf);
 
+        [Post("/api/costsAndCharges/for-instrument")]
         Task<CostsAndChargesCalculationContract[]> GenerateForInstrument(string instrument, bool withOnBehalf);
 
+        [Get("/api/costsAndCharges")]
         Task<PaginatedResponseContract<CostsAndChargesCalculationContract>> Search(string accountId, string
                 instrument, decimal? quantity, OrderDirectionContract? direction, DateTime? from, DateTime? to,
             int? skip, int? take, bool isAscendingOrder = true);
         
+        [Get("/api/costsAndCharges/by-ids")]
         Task<CostsAndChargesCalculationContract[]> Get(string[] ids);
     }
 }
