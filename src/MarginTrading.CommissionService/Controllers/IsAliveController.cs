c42a0b1b8b3c7f7289c3269f3e1a6362205274c2
Nika Gamkrelidze <nikoloz.gamkrelidze@lykke-business.ch>
2020-03-31 20:15:59 +0400

    LT-2165_migrate-3.1-update-dl

--- src/MarginTrading.CommissionService/Controllers/IsAliveController.cs
+++ src/MarginTrading.CommissionService/Controllers/IsAliveController.cs
@@ -19,7 +19,6 @@ namespace MarginTrading.CommissionService.Controllers
         /// </summary>
         /// <returns></returns>
         [HttpGet]
-        [SwaggerOperation("IsAlive")]
         [ProducesResponseType(typeof(IsAliveResponse), (int) HttpStatusCode.OK)]
         public IActionResult Get()
         {
--- src/MarginTrading.CommissionService/Infrastructure/CustomOperationIdOperationFilter.cs
+++ src/MarginTrading.CommissionService/Infrastructure/CustomOperationIdOperationFilter.cs
@@ -2,6 +2,7 @@
 // See the LICENSE file in the project root for more information.
 
 using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Swagger;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
@@ -9,7 +10,7 @@ namespace MarginTrading.CommissionService.Infrastructure
 {
     public class CustomOperationIdOperationFilter : IOperationFilter
     {
-        public void Apply(Operation operation, OperationFilterContext context)
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
         {
             var actionDescriptor = (ControllerActionDescriptor) context.ApiDescription.ActionDescriptor;
             operation.OperationId = actionDescriptor.ControllerName + actionDescriptor.ActionName;
