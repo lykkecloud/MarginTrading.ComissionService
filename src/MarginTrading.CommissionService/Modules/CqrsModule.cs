1b4ceb840dbef87b85e7488ec840e43dd2e0badf
Andrei Tarutin <tarutin_andrew@mail.ru>
2020-05-07 10:36:42 +0300

    updated packages

--- src/MarginTrading.CommissionService/Modules/CqrsModule.cs
+++ src/MarginTrading.CommissionService/Modules/CqrsModule.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) 2019 Lykke Corp.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Collections.Generic;
 using Autofac;
 using Common.Log;
@@ -61,7 +62,7 @@ namespace MarginTrading.CommissionService.Modules
 
             var rabbitMqSettings = new RabbitMQ.Client.ConnectionFactory
             {
-                Uri = _settings.ConnectionString
+                Uri = new Uri(_settings.ConnectionString, UriKind.Absolute)
             };
 
             var messagingEngine = new MessagingEngine(_log, new TransportResolver(
