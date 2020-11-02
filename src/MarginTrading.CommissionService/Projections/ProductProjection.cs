5940c992912bb3afc5e244e03eb56d8a87dc6454
Gennady Ponomarev <gennady.ponomarev@lykke-business.ch>
2020-11-02 11:21:32 +0500

    LT-2733: ignore not started products

--- src/MarginTrading.CommissionService/Projections/ProductProjection.cs
+++ src/MarginTrading.CommissionService/Projections/ProductProjection.cs
@@ -1,8 +1,10 @@
 // Copyright (c) 2019 Lykke Corp.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
+using MarginTrading.AssetService.Contracts.Enums;
 using MarginTrading.AssetService.Contracts.Products;
 using MarginTrading.CommissionService.Core.Services;
 
@@ -20,6 +22,19 @@ namespace MarginTrading.CommissionService.Projections
         [UsedImplicitly]
         public async Task Handle(ProductChangedEvent @event)
         {
+            switch (@event.ChangeType)
+            {
+                case ChangeType.Creation:
+                case ChangeType.Edition:
+                    if (!@event.NewValue.IsStarted) return;
+                    break;
+                case ChangeType.Deletion:
+                    if (!@event.OldValue.IsStarted) return;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            
             _cacheUpdater.InitAssetPairs();
             _cacheUpdater.InitAssets();
             _cacheUpdater.InitTradingInstruments();
