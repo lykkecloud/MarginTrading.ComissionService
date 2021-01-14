912169289dcfe255bcc36f8bff15cd66d338ed3d
Andrey Anokhin <andrey.anokhin@lykke.com>
2021-01-14 16:58:14 +0300

    LT-2984: use 0 interest rates for a configured asset types

--- src/MarginTrading.CommissionService.Services/CommissionCalcService.cs
+++ src/MarginTrading.CommissionService.Services/CommissionCalcService.cs
@@ -12,6 +12,7 @@ using MarginTrading.CommissionService.Core;
 using MarginTrading.CommissionService.Core.Caches;
 using MarginTrading.CommissionService.Core.Domain;
 using MarginTrading.CommissionService.Core.Services;
+using MarginTrading.CommissionService.Core.Settings;
 using MarginTrading.TradingHistory.Client;
 using MarginTrading.TradingHistory.Client.Models;
 using Newtonsoft.Json;
@@ -28,6 +29,7 @@ namespace MarginTrading.CommissionService.Services
         private readonly IAccountRedisCache _accountRedisCache;
         private readonly ILog _log;
         private readonly IInterestRatesCacheService _interestRatesCacheService;
+        private readonly CommissionServiceSettings _settings;
 
         public CommissionCalcService(
             ICfdCalculatorService cfdCalculatorService,
@@ -37,7 +39,8 @@ namespace MarginTrading.CommissionService.Services
             IOrderEventsApi orderEventsApi,
             IAccountRedisCache accountRedisCache,
             ILog log,
-            IInterestRatesCacheService interestRatesCacheService)
+            IInterestRatesCacheService interestRatesCacheService,
+            CommissionServiceSettings settings)
         {
             _cfdCalculatorService = cfdCalculatorService;
             _assetsCache = assetsCache;
@@ -47,6 +50,7 @@ namespace MarginTrading.CommissionService.Services
             _accountRedisCache = accountRedisCache;
             _log = log;
             _interestRatesCacheService = interestRatesCacheService;
+            _settings = settings;
         }
 
         /// <summary>
@@ -61,16 +65,22 @@ namespace MarginTrading.CommissionService.Services
 
             var calculationBasis = fxRate * Math.Abs(volume) * closePrice;
 
-            var variableRateBase = _interestRatesCacheService.GetRate(rateSettings.VariableRateBase);
-            var variableRateQuote = _interestRatesCacheService.GetRate(rateSettings.VariableRateQuote);
-
             var financingRate = -rateSettings.FixRate
                                 - (direction == PositionDirection.Short
                                     ? rateSettings.RepoSurchargePercent
-                                    : 0)
-                                + (variableRateBase - variableRateQuote)
-                                * (direction == PositionDirection.Long ? 1 : -1);
+                                    : 0);
+            
+            var assetPair = _assetPairsCache.GetAssetPairById(instrument);
 
+            if (!_settings.AssetTypesWithZeroInterestRates.Contains(assetPair.AssetType))
+            {
+                var variableRateBase = _interestRatesCacheService.GetRate(rateSettings.VariableRateBase);
+                var variableRateQuote = _interestRatesCacheService.GetRate(rateSettings.VariableRateQuote);
+
+                financingRate += (variableRateBase - variableRateQuote)
+                                 * (direction == PositionDirection.Long ? 1 : -1);
+            }
+            
             var dayFactor = (decimal) numberOfFinancingDays / financingDaysPerYear;
 
             return (Math.Round(calculationBasis * financingRate * dayFactor,
